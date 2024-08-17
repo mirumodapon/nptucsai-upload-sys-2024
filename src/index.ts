2d14260b8f184a3418410c37ee06f6c1b1508a50
Mirumo <mysteryperson520@gmail.com>
2024-08-17 14:18:28 +0800

    Feat: auth mysql and connect redis

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,15 @@
 import App from '@/app';
 import { APP_PORT } from '@/config';
 
-const app = new App({ routes: [] });
-app.listen(APP_PORT);
+import mysql from '@/database';
+import redis from '@/database/redis';
+
+async function main() {
+  await mysql.authenticate();
+  await redis.connect();
+
+  const app = new App({ routes: [] });
+  app.listen(APP_PORT);
+}
+
+main();
