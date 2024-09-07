a678bb8e5ed9ff46b0a6ed40ed4086f8e72deeb0
Mirumo <mysteryperson520@gmail.com>
2024-09-08 00:04:11 +0800

    Feat(frontend): add react query

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,17 +1,32 @@
-import { Fragment } from 'react';
 import { RouterProvider } from 'react-router-dom';
 import ToastProvider from '@/components/features/toast/ToastProvider';
 import router from '@/router';
 import useTheme from '@/hooks/useTheme';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      retry: 0,
+      staleTime: 60 * 1000,
+      gcTime: 3 * 60 * 1000,
+      refetchInterval: false,
+      refetchOnMount: false,
+      refetchOnWindowFocus: false
+    }
+  }
+});
 
 function App() {
   useTheme();
 
   return (
-    <Fragment>
+    <QueryClientProvider client={queryClient}>
       <RouterProvider router={router} />
       <ToastProvider />
-    </Fragment>
+      <ReactQueryDevtools />
+    </QueryClientProvider>
   );
 }
 
--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,13 +1,25 @@
 import { resolve } from 'node:path';
-import { defineConfig } from 'vite';
+import { defineConfig, UserConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  resolve: {
-    alias: {
-      '@': resolve(__dirname, 'src')
+export default defineConfig(function ({ mode }) {
+  const env = loadEnv(mode, process.cwd(), '');
+
+  const config: UserConfig = {
+    plugins: [react()],
+    resolve: {
+      alias: {
+        '@': resolve(__dirname, 'src')
+      }
     }
+  };
+
+  if (mode === 'development') {
+    config.server = {
+      proxy: { '/api': { target: env.VITE_BASE_URL, changeOrigin: true } }
+    };
   }
+
+  return config;
 });
